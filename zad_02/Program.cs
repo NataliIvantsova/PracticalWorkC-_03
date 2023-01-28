9db5fa2c0d8bc7b69a2cac18c58be044b54c40a2
NataliIvantsova <ivancovanm@yandex.ru>
2023-01-29 02:08:49 +0300

    Задача 02 получилась Ура

---
+++ zad_02/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 2: Напишите программу, 
+// которая принимает на вход число и выдаёт сумму цифр в числе. 
+// Реализовать через функции.
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int SumOfDigits(int n)
+{
+    int sum = 0;
+    int ostatok = 0;
+    while (n >0)
+    {
+        if(n%10!=0)
+        {
+            ostatok=n%10;
+        }
+        else
+        {
+            ostatok=n/10;
+        }
+        sum = sum + ostatok;
+        n=n/10;
+    }
+    return sum;
+}
+int number = Prompt("Введите число");
+
+int digital = SumOfDigits(number);
+System.Console.WriteLine($"{digital}");
