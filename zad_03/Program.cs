815a486d20eb875a6fd686361cc8678e73f8f24f
NataliIvantsova <ivancovanm@yandex.ru>
2023-01-29 03:36:30 +0300

    Задача 3. решала долго. не уверена

---
+++ zad_03/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 3: Напишите программу, которая выводит массив 
+// из 8 элементов, заполненный случайными числами.
+// Оформите заполнение массива и вывод в виде функции 
+// (пригодится в следующих задачах). Реализовать через 
+// функции. (* Доп сложность, “введите количество 
+// элементов массива”, “Введите минимальный порог 
+// случайных значений”, “Введите максимальный порог 
+// случайных значений”)
+
+ int Prompt(string message)
+{
+System.Console.Write($"{message}: ");
+int result = Convert.ToInt32(Console.ReadLine());
+return result;
+}
+int[] CreatArray(int X)
+{
+int[] array = new int[X];
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next();
+}
+return array;
+}
+void ShowArray(int[] array)
+{
+for (int j = 0; j < array.Length; j++)
+{
+Console.Write($"{array[j]}");
+}
+}
+
+int X = Prompt("введите количество элементов массива ");
+int [] array = CreatArray(X);
+ShowArray (array);
---
+++ zad_031/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+void FillArrya(int [] massiv)
+{
+    int length = 8; // ищем длину массива (переменная length) через инструмент Length
+    for (int i = 0; i < length; i++)
+    {
+        massiv [i] = new Random().Next(1,3); 
+    }
+
+} 
+
+void PrintArrya(int[] col)// метод который будет печатать массив
+{
+    int count = 8; //количество элементов массива col
+    for (int j = 0; j < count; j++)
+    {
+      Console.WriteLine(col[j]);//будем выводить на экран(консоль) значение текущего элемента
+    }
+  
+}
+
+int[] array = new int[8];// массив arrya ссодержит 8 элементов || 
+// //new int[8] -  создай новый массив в котором будет 8 элементов/ 
+
+FillArrya(array);//таким образом мы определили массив из 8 элементов
+
+PrintArrya(array); //PrintArrya(array); печатает массива из 8 чисел
